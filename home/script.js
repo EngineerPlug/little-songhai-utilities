0b757b23a86f36972cbce6b3978004310ef4f9f6
Martin Clark <webdeveloperplug@gmail.com>
2022-09-23 21:09:45 -0500

    Another little script.js update for the enter button.

--- home/script.js
+++ home/script.js
@@ -1,11 +1,16 @@
 const twitter = document.getElementById("twitter-btn");
 const facebook = document.getElementById("facebook-btn");
+const enter = document.getElementById("enter");
 
+function enterHere() {
+    window.location.href = "https://MClarkSoftwareEngineer.github.io/little-songhai-utilities/coffee/";
+}
 function sendToTwitter() {
     window.location.href = "https://twitter.com/Little_Songhai";
 }
 function sendToFaceBook() {
-    window.location.href = "https://www.facebook.com/MobileAfricanCoffee"
+    window.location.href = "https://www.facebook.com/MobileAfricanCoffee";
 }
+enter.addEventListener('click', enterHere);
 facebook.addEventListener('click', sendToFaceBook);
 twitter.addEventListener('click', sendToTwitter);

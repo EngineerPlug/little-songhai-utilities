0dff692057c90ec619cacf7fdb9e799b6285e3bb
Martin Clark <webdeveloperplug@gmail.com>
2022-09-23 20:52:48 -0500

    Another quick script.js update for the home team.

--- home/script.js
+++ home/script.js
@@ -4,5 +4,8 @@ const facebook = document.getElementById("facebook-btn");
 function sendToTwitter() {
     window.location.href = "https://twitter.com/Little_Songhai";
 }
-
+function sendToFaceBook() {
+    window.location.href = "https://www.facebook.com/MobileAfricanCoffee"
+}
+facebook.addEventListener('click', sendToFaceBook);
 twitter.addEventListener('click', sendToTwitter);
